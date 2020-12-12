64a1972df74fac95aa0a30f8df26509eadf196ec
anjalika-nande <35534786+anjalika-nande@users.noreply.github.com>
2020-12-12 16:19:36 -0500

    Update networks.py
    Added another network creation code that can have different amounts of mixing between the clusters

--- networks.py
+++ networks.py
@@ -130,6 +130,73 @@ def create_external_corr(pop,pop_subset,degree_dist,n,r,indx_list,correlation_gr
                 cols.extend([j,i])
                 data.extend([1,1])
     return [rows, cols, data]
+
+def create_external_different_mixing(pop_subset, p_mixing, degree_dist):
+  """ Create external connections where each person has an average probability of interaction with other people in the population depending upon their neighborhood/cluster.
+  @param pop_subset: Contains the individual indices of people belonging to each neighborhood
+  @type pop_subset: List of n lists where n is the number of neighborhoods
+  @param p_mixing: Average probability of having external contacts in each neighborhood
+  @type p_mixing: List of n lists where n is the number of neighborhoods
+  @param degree_dist: Contains the number of external connections for each individual
+  @type degree_dist: List of n lists where n is the number of neighborhoods
+  @return: Sparse adjacency matrix
+  @type: List of lists [rows, cols, data]
+  """
+  
+    no_neigh = len(pop_subset)
+    correlation = [[] for i in range(no_neigh)]
+    rows = []
+    cols = []
+    data = []
+  
+    # For each neighborhood in cyclic order for eg. 1-1, 1-2, 1-3, 2-1, 2-2, 2-3, 3-1, 3-2, 3-3
+    stub_types = [[[],[],[]] for i in range(no_neigh)]
+
+    np2.random.seed(789)
+    for neigh in range(no_neigh):
+      for i in range(len(pop_subset[neigh])):
+       # For eg: 0 - stays in the same neigh, 1 - goes to the next in cyclic order etc
+        stubs = np2.random.choice([j for j in range(no_neigh)], p=r_list[neigh], size = int(degree_dist[neigh][i]))
+        stub_indx, stub_counts = np2.unique(stubs, return_counts=True)
+        for unique in range(len(stub_counts)):
+          stub_types[neigh][stub_indx[unique]].extend([pop_subset[neigh][i] for j in range(stub_counts[unique])])
+
+    # Within cluster connections
+    for neigh in range(no_neigh):
+      within = stub_types[neigh][neigh]
+      within_pairs = np2.random.choice(within, size = (int(len(within)/2),2), replace = False)
+      for pairs in range(len(within_pairs)):
+        i = within_pairs[pairs][0]
+        j = within_pairs[pairs][1] 
+        rows.extend([i,j])
+        cols.extend([j,i])
+        data.extend([1,1])
+
+    # Outside cluster connections 
+    diff_cluster_connections = list(itertools.combinations([j for j in range(no_neigh)],2))
+    for neigh1, neigh2 in diff_cluster_connections:
+      stub1 = stub_types[neigh1][neigh2]
+      stub2 = stub_types[neigh2][neigh1]
+      stub1.extend(stub2)
+      between_neigh = sorted(stub1)
+      n_q = math.ceil(len(between_neigh)/2)
+      n_quantiles = [between_neigh[i:i + n_q] for i in range(0, len(between_neigh), n_q)]
+
+      for quantile in range(math.ceil(len(n_quantiles)/2)):
+        pair_one = np2.random.choice(n_quantiles[quantile], size = len(n_quantiles[quantile]), replace = False)
+        pair_two = np2.random.choice(n_quantiles[-1-quantile], size = len(n_quantiles[-1-quantile]), replace = False)
+        if len(pair_one) > len(pair_two):
+          no_pairs = len(pair_two)
+        else:
+          no_pairs = len(pair_one)
+        for pairs in range(no_pairs):
+          i = pair_one[pairs]
+          j = pair_two[pairs]
+          rows.extend([i,j])
+          cols.extend([j,i])
+          data.extend([1,1])
+
+    return [rows,cols,data]
     
 def create_friend_groups(para,age_grp_size,indices):
     """ Create age dependent distributions of sizes of friend groups and assign individuals to them
